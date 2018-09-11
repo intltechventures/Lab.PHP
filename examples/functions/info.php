58f4e06bcc101546d994dab8c74fb1dd094f2cb5
Kelvin D. Meeks <kmeeks@intltechventures.com>
2018-09-11 11:29:41 -0700

    moved to functions dir

---
+++ examples/functions/info.php
@@ -0,0 +1,13 @@
+<?php
+// Source: 
+// http://www.php.net/manual/en/function.phpinfo.php
+//
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+// Show just the module information.
+// phpinfo(8) yields identical results.
+phpinfo(INFO_MODULES);
+
+?>
+
